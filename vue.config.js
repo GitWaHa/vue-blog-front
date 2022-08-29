8414aa7dbf7f37bb003aff42b809c68d505e1fb9
wahaoo-local <wa.haoo@outlook.com>
2022-08-29 14:37:34 +0000

    test

--- vue.config.js
+++ vue.config.js
@@ -42,6 +42,9 @@ module.exports = {
     transpileDependencies: [
         'vuetify'
     ],
+    devServer: {
+        disableHostCheck: true,
+    },
     configureWebpack: {
         // plugins: [
         //   new CopyWebpackPlugin({
